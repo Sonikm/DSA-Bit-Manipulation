f336fa0a42f07bcc0ae23c8928b05b3cae72fa41
Soni <sonikm443@gmail.com>
2023-05-12 10:39:22 +0530

    Second approach

--- complement_of_base10_integer.cpp
+++ complement_of_base10_integer.cpp
@@ -1,18 +1,43 @@
 #include<iostream>
 using namespace std;
 
+int find1sComplement(int n) {
+   
+  /*....................Approach 1.................................*/
+   if(n == 0) return 1;
+//    int x = 1;
+ 
+//    while(x <= n){
+//     n = n^x;
+//     x <<=1;
+//    } 
+  
+//   return n;
+
+  /*.........................Approach 2.................................*/
+
+  int m = n;
+  int marks = 0;
+
+  while (m)
+  {
+    marks = (marks << 1) | 1;
+    m >>= 1;
+  }
+
+   int ans = (~n) & marks;
+   return  ans;
+}
+
 int main()
 {
     
-   int n = 10;
-   int x = 1;
+   int n;
 
-   while(x <= n){
-    n = n^x;
-    x <<=1;
-   } 
+   cout << "Enter input: ";
+   cin >> n;
 
-   cout << "Output: " << n << endl;
+   cout << "Output: " << find1sComplement(n) << endl;
 
    return 0;
 }
